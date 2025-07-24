8ce996b2c55a9488f2d13bb42a5d431a23ab81fc
Chandra Sekhar <chandrasekhar4321n@gmail.com>
2025-07-24 08:56:32 +0000

    updated the code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,41 @@
 import { WebSocket, WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 })
-interface user {
-  socket : WebSocket ; 
-  rommId : string ; 
+interface obj {
+  roomId:string , 
+  allsockets : WebSocket[]
 }
-const users :user[]= [];
+var allroomInfo :obj[]= [];
+  allroomInfo.push({
+    roomId: "redroom",
+    allsockets: []
+  })
+  
 wss.on("connection", (socket) => {
-  console.log("connected to webscoket");
+   if(!allroomInfo[0].allsockets.find((soc)=> socket == soc)){
+    allroomInfo[0].allsockets.push(socket);
+   }
   socket.on("message", (e) => {
-    console.log(e.toString());
-         socket.send(e.toString());
+    let parsedmessage  ;
+    try{
+         parsedmessage = JSON.parse(e.toString());
+    }catch(error){
+       console.error("cannot parse the json properly");
+       return ;
+    }
+      
+      if(parsedmessage.type == "message"){
+         const currentusers = allroomInfo.find((cur)=> cur.roomId == parsedmessage.roomId)
+         currentusers?.allsockets.forEach((eachsocket)=>
+         {
+          if(eachsocket !== socket){
+            eachsocket.send(parsedmessage.message);
+          }
+         }
+          )
+      }
       })
+
+      socket.on("close" ,()=>{
+          allroomInfo[0].allsockets = allroomInfo[0].allsockets.filter((soc) => soc !== socket)
+})
   })
